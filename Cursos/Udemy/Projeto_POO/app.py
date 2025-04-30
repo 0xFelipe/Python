dd8ea7d74ba0c33421fb868d127347af38712db7
0xFelipe <felipe-souza-mendes@outlook.com>
2025-04-30 14:47:31 -0300

    Atualização

--- Cursos/Udemy/Projeto_POO/app.py
+++ Cursos/Udemy/Projeto_POO/app.py
@@ -7,6 +7,9 @@ biblioteca_cidade.alterna_estado()
 biblioteca_shopping.alterna_estado()
 
 biblioteca_cidade.receber_avaliacao("Cliente 1", 5)
+biblioteca_cidade.receber_avaliacao("Cliente 2", 4)
+biblioteca_shopping.receber_avaliacao("Cliente 3", 3)
+
 
 def main():
     Biblioteca.listar_bibliotecas()
