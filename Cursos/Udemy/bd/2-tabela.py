9dfd0b4d6482bdb167d6f5c12efca1ddc7a9842e
0xFelipe <felipe-souza-mendes@outlook.com>
2025-05-06 15:48:03 -0300

    Criando um novo projeto, criação de tabela,  conexão com a tabela, criação de colunas

---
+++ Cursos/Udemy/bd/2-tabela.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+# 1 -  Conectando ao banco de dados
+conexao = sqlite3.connect('titulo.db')
+
+# 2 - Criando o cursor
+cursor = conexao.cursor()
+
+# 3 - Criando a tabela
+cursor.execute(
+    """
+        CREATE TABLE filmes(
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            titulo TEXT NOT NULL,
+            ano INTEGER NOT NULL,
+            nota REAL NOT NULL   
+        );
+    
+    """
+)
+
+# 4 - Fecha a conexão
+conexao.close()
+print('Tabela criada com sucesso!')
